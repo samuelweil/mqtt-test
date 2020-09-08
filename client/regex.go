fa6651f834dd667cbf60af951077009f5ceb86af
Sam Weil <samuel.weil@molex.com>
2020-09-08 15:20:41 -0500

    Add regex to capture topic parameters

---
+++ client/regex.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+)
+
+var pathConvert = regexp.MustCompile("{(.*?)}")
+
+func convertPath(path string) string {
+	return pathConvert.ReplaceAllStringFunc(path, func(ident string) string {
+		return fmt.Sprintf("(?P<%s>.*)", ident[1:len(ident)-1])
+	})
+}
---
+++ client/regex_test.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"testing"
+)
+
+func checkString(t *testing.T, exp string, got string) {
+	if exp != got {
+		t.Errorf("Expected \"%s\", got \"%s\"", exp, got)
+	}
+}
+
+func TestPathRegex(t *testing.T) {
+	result := pathConvert.FindAllStringSubmatch("{first}/{second}/{third}", -1)
+
+	if len(result) != 3 {
+		t.Fatalf("Regex failed to return 4 values, got %d", len(result))
+	}
+
+	checkString(t, "first", result[0][1])
+	checkString(t, "second", result[1][1])
+	checkString(t, "third", result[2][1])
+}
+func TestConvertPath(t *testing.T) {
+
+	tests := []struct {
+		inp, exp string
+	}{
+		{
+			"/{first}/{second}/{third}",
+			"/(?P<first>.*)/(?P<second>.*)/(?P<third>.*)",
+		},
+	}
+
+	for _, test := range tests {
+		result := convertPath(test.inp)
+		if result != test.exp {
+			t.Errorf("Expected \"%s\", got \"%s\"", test.exp, result)
+		}
+	}
+
+}
